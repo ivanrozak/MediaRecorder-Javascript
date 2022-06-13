cba836d0ee2b6e6bcaaca1c3b8ea80570e5d4264
ivanrozak <ivan.rozak@yesdok.com>
2022-06-13 13:11:55 +0700

    user agent

--- src/components/UserAgent.jsx
+++ src/components/UserAgent.jsx
@@ -2,9 +2,26 @@ import React, {useState, useEffect} from 'react'
 
 export default function UserAgent() {
   const [userAgent, setUserAgent] = useState('')
+
+  const ua = navigator.userAgent
+
   useEffect(() => {
-    setUserAgent(navigator.userAgent)
+    // setUserAgent(navigator.userAgent)
+    if ((/(i[PSa-z\s]+);.*?CPU\s([OSPa-z\s]+(?:([\d_]+)|;))/g.test(ua)) || (/version\/([\w\.]+) .*(mobile ?safari|safari)/i.test(ua)) || (/version\/([\w\.]+) .*mobile\/\w+ (safari)/i.test(ua))) {
+      setUserAgent('is Safari or Ipad')
+    } else {
+      setUserAgent('is not Safari or Ipad')
+    }
   }, [])
+
+  useEffect(() => {
+    if (navigator.onLine) {
+      console.log('online')
+    } else {
+      console.log('offline')
+    }
+  }, [])
+
   return (
     <>
       <div>UserAgent</div>
